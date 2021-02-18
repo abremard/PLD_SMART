e0817d62572ed6db709f1b9e3c95728bebe0cbb6
Alex <bremard.alexandre@gmail.com>
2021-02-18 16:55:55 +0100

    initial commit

---
+++ src/MuseGAN/train.py
@@ -0,0 +1,90 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import types
+
+from models.MuseGAN import MuseGAN
+from utils.loaders import load_music
+
+from music21 import midi
+from music21 import note, stream, duration
+
+# run params
+SECTION = 'compose'
+RUN_ID = '001'
+DATA_NAME = 'chorales'
+FILENAME = 'Jsb16thSeparated.npz'
+RUN_FOLDER = 'run/{}/'.format(SECTION)
+RUN_FOLDER += '_'.join([RUN_ID, DATA_NAME])
+
+if not os.path.exists(RUN_FOLDER):
+    os.mkdir(RUN_FOLDER)
+    os.mkdir(os.path.join(RUN_FOLDER, 'viz'))
+    os.mkdir(os.path.join(RUN_FOLDER, 'images'))
+    os.mkdir(os.path.join(RUN_FOLDER, 'weights'))
+    os.mkdir(os.path.join(RUN_FOLDER, 'samples'))
+
+mode =  'build' # ' 'load' #
+
+BATCH_SIZE = 64
+n_bars = 2
+n_steps_per_bar = 16
+n_pitches = 84
+n_tracks = 4
+
+data_binary, data_ints, raw_data = load_music(DATA_NAME, FILENAME, n_bars, n_steps_per_bar)
+data_binary = np.squeeze(data_binary)
+
+gan = MuseGAN(input_dim = data_binary.shape[1:]
+        , critic_learning_rate = 0.001
+        , generator_learning_rate = 0.001
+        , optimiser = 'adam'
+        , grad_weight = 10
+        , z_dim = 32
+        , batch_size = BATCH_SIZE
+        , n_tracks = n_tracks
+        , n_bars = n_bars
+        , n_steps_per_bar = n_steps_per_bar
+        , n_pitches = n_pitches
+        )
+
+if mode == 'build':
+    gan.save(RUN_FOLDER)
+else:                 
+    gan.load_weights(RUN_FOLDER)
+
+gan.chords_tempNetwork.summary()
+
+gan.barGen[0].summary()
+
+gan.generator.summary()
+
+gan.critic.summary()
+
+EPOCHS = 50
+PRINT_EVERY_N_BATCHES = 10
+
+gan.epoch = 0
+
+gan.train(     
+    data_binary
+    , batch_size = BATCH_SIZE
+    , epochs = EPOCHS
+    , run_folder = RUN_FOLDER
+    , print_every_n_batches = PRINT_EVERY_N_BATCHES
+)
+
+fig = plt.figure()
+plt.plot([x[0] for x in gan.d_losses], color='black', linewidth=0.25)
+
+plt.plot([x[1] for x in gan.d_losses], color='green', linewidth=0.25)
+plt.plot([x[2] for x in gan.d_losses], color='red', linewidth=0.25)
+plt.plot(gan.g_losses, color='orange', linewidth=0.25)
+
+plt.xlabel('batch', fontsize=18)
+plt.ylabel('loss', fontsize=16)
+
+plt.xlim(0, len(gan.d_losses))
+# plt.ylim(0, 2)
+
+plt.show()
