392ae9acdadb86fa9fbfc392cf515860beadc2f0
Bremard Alexandre <bremard.alexandre@gmail.com>
2021-04-22 00:20:06 +0200

    lstm data

--- src/Algo/MuseGAN/v0/__init__.py
+++ src/Algo/MuseGAN/v0/__init__.py
@@ -1,2 +1,8 @@
 """ MuseGAN
 """
+
+from pathlib import Path
+parent = Path(__file__).resolve().parent
+srcPath = str(parent.parent.parent.parent).replace("\\", "\\\\")
+import sys
+sys.path.insert(0, srcPath)
