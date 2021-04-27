bf57b81294c2fdf35e909a4f8829b8b56ef87f06
thestefanristovski <stefan.ristovski1@gmail.com>
2021-04-27 11:43:33 +0200

    result tile complete with all states

--- front/src/components/ResultTile.js
+++ front/src/components/ResultTile.js
@@ -1,26 +1,48 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import midi from "../images/midi.png"
+import { useLoading, Grid } from '@agney/react-loading';
 
 export default function ResultTile({isLoading, hasResult, fileName, downloadLink}) {
+
+    const selectedStyle = {
+        backgroundColor: '#6EC3F4',
+    }
 
     if (!isLoading) {
         if(hasResult) {
             //result with all info
+            return (
+                <div className="tile">
+                    <h4>Result</h4>
+                    <div className="metadata">
+                        <h5>{fileName}</h5>
+                        <p>0:15</p>
+                    </div>
+                    <a>Download .midi</a>
+                </div>
+            )
         } else {
-            //empty result
+            return(
+                <div className="tile">
+                    <h4>Result</h4>
+                    <div className="metadata">
+                        <h5>Nothing to see here...</h5>
+                        <p>Generate a piece and it will appear here</p>
+                    </div>
+                </div>
+            )
         }
     } else {
         //loading animation
-    }
-    return (
-        <div className="tile">
+        return(
+            <div className="tile" style={selectedStyle}>
             <h4>Result</h4>
             <div className="metadata">
-                <h5>{fileName}</h5>
-                <p>0:15</p>
+                <h5>Generating...</h5>
             </div>
-            <a>Download .midi</a>
+            <Grid width="60"/>
         </div>
-    )
+        )
+    }
 }
