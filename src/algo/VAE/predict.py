c239aed58fd474a8a2a2209554643c724070d444
Bremard Alexandre <bremard.alexandre@gmail.com>
2021-04-29 19:12:21 +0200

    predict params update

--- src/algo/VAE/predict.py
+++ src/algo/VAE/predict.py
@@ -6,22 +6,20 @@ from algo.VAE.models.VAE import play, interpolate, download
 
 import tensorflow.compat.v1 as tf
 
-BASE_DIR = "gs://download.magenta.tensorflow.org/models/music_vae/colab2"
-
 def generate(
     input_mel_midi_data = [
         tf.io.gfile.GFile(fn, 'rb').read()
-        for fn in sorted(tf.io.gfile.glob(BASE_DIR + '/midi/mel_2bar*.mid'))]):
+        for fn in sorted(tf.io.gfile.glob('gs://download.magenta.tensorflow.org/models/music_vae/colab2/midi/mel_2bar*.mid'))],
+    temperature = 0.5,
+    num_steps=15,
+    start_melody=0,
+    end_melody=1):
     
-    #@title Load the pre-trained model.
+    # Load the pre-trained model.
     mel_2bar_config = configs.CONFIG_MAP['cat-mel_2bar_big']
-    mel_2bar = TrainedModel(mel_2bar_config, batch_size=4, checkpoint_dir_or_path=BASE_DIR + '/checkpoints/mel_2bar_big.ckpt')
-    #@title Option 1: Use example MIDI files for interpolation endpoints.
-    input_mel_midi_data = [
-        tf.io.gfile.GFile(fn, 'rb').read()
-        for fn in sorted(tf.io.gfile.glob(BASE_DIR + '/midi/mel_2bar*.mid'))]
+    mel_2bar = TrainedModel(mel_2bar_config, batch_size=4, checkpoint_dir_or_path='gs://download.magenta.tensorflow.org/models/music_vae/colab2/checkpoints/mel_2bar_big.ckpt')
 
-    #@title Extract melodies from MIDI files. This will extract all unique 2-bar melodies using a sliding window with a stride of 1 bar.
+    # Extract melodies from MIDI files. This will extract all unique 2-bar melodies using a sliding window with a stride of 1 bar.
     mel_input_seqs = [mm.midi_to_sequence_proto(m) for m in input_mel_midi_data]
     extracted_mels = []
     for ns in mel_input_seqs:
@@ -32,16 +30,7 @@ def generate(
         print("Melody", i)
         play(ns)
         
-    #@title Interpolate between 2 melodies, selected from those in the previous cell.
-    start_melody = 0 #@param {type:"integer"}
-    end_melody = 1 #@param {type:"integer"}
     start_mel = extracted_mels[start_melody]
     end_mel = extracted_mels[end_melody]
 
-    temperature = 0.5 #@param {type:"slider", min:0.1, max:1.5, step:0.1}
-    num_steps = 13 #@param {type:"integer"}
-
     mel_2bar_interp = interpolate(mel_2bar, start_mel, end_mel, num_steps=num_steps, temperature=temperature)
-    
-    #@title Optionally download interpolation MIDI file.
-    download(mel_2bar_interp, 'mel_2bar_interp.mid')
