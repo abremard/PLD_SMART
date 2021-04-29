dbe0038786fb945321a5adf7a27bb386ddf59fc1
thestefanristovski <stefan.ristovski1@gmail.com>
2021-04-29 11:32:54 +0200

    text replacing the lorem ipsum

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -53,16 +53,17 @@ export default function Home() {
 
         </div>
         <div className="text">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat viverra molestie quam faucibus viverra
-                nisl. Vitae eget risus, auctor viverra pharetra. Consequat, cras amet, dolor, varius lectus odio libero,
-                leo. Ultrices tristique ullamcorper fermentum lacus diam proin sagittis, ac.</p>
+            <p>Creating music just got much easier. Ilolio Music Lab offers you creation tools
+            that help you find inspiration, generate new ideas, create unique compositions
+            for your musical projects, or play with music. Ilolio is your ultimate co-writer, co-producer and co-musical genius.</p>
         </div>
         <div className="works">
             <h3>How it works</h3>
             <img src={illustration}/>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat viverra molestie quam faucibus
-                    viverra nisl. Vitae eget risus, auctor viverra pharetra. Consequat, cras amet, dolor, varius lectus
-                    odio libero, leo. Ultrices tristique ullamcorper fermentum lacus diam proin sagittis, ac.</p>
+                <p>Ilolio Music lab uses a combination of multiple machine learning algorithms to offer you multiple
+                music creation tools. The algorithms take into account your own files and customizations of different
+                parameters to create unique musical compositions. Anyone can create with Ilolio - the simple interface
+                is easy to navigate and understand.</p>
         </div>
         <div className="services">
             <h3>What can you do</h3>
@@ -70,31 +71,27 @@ export default function Home() {
                 <tr>
                     <td>
                         <img src={scratch}/>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat viverra molestie quam
-                                faucibus viverra nisl. Vitae eget risus, auctor viverra pharetra. Consequat, cras amet,
-                                dolor, varius lectus odio libero, leo. Ultrices tristique ullamcorper fermentum lacus
-                                diam proin sagittis, ac.</p>
+                            <p>Create a completely new musical piece with one or multiple instruments. Choose
+                                between multiple musical styles, artists of inspiration, and types of instruments
+                                to make every unique piece your own. Uses the MuseGAN and LSTM algorithms.</p>
                     </td>
                     <td>
                         <img src={together}/>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat viverra molestie quam
-                                faucibus viverra nisl. Vitae eget risus, auctor viverra pharetra. Consequat, cras amet,
-                                dolor, varius lectus odio libero, leo. Ultrices tristique ullamcorper fermentum lacus
-                                diam proin sagittis, ac.</p>
+                            <p>Bring your favorite two musical pieces together. Upload two midi files and our
+                                algorithm will interpolate and create two original mash-ups of your uploaded music. Choose
+                                the length and the balance of your mashup for the perfect result.</p>
                     </td>
                     <td>
                         <img src={match}/>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat viverra molestie quam
-                                faucibus viverra nisl. Vitae eget risus, auctor viverra pharetra. Consequat, cras amet,
-                                dolor, varius lectus odio libero, leo. Ultrices tristique ullamcorper fermentum lacus
-                                diam proin sagittis, ac.</p>
+                            <p>Create a completely new musical piece using your own favorite sounds as the inspiration.
+                                Upload up to 10 midi files of your choosing - any style, any artist, any instrument. Ilolio
+                                will create a brand new midi file inspired by your uploads. Uses the LSTM algorithm.</p>
                     </td>
                     <td>
                         <img src={switchimg}/>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat viverra molestie quam
                                 faucibus viverra nisl. Vitae eget risus, auctor viverra pharetra. Consequat, cras amet,
-                                dolor, varius lectus odio libero, leo. Ultrices tristique ullamcorper fermentum lacus
-                                diam proin sagittis, ac.</p>
+                                dolor, varius lectus odio libero, leo. Ultrices tristique.</p>
                     </td>
                 </tr>
             </table>
@@ -102,6 +99,10 @@ export default function Home() {
         <div className="ready">
             <h3>Ready?</h3>
             <Link to="/studio">Let's Go!</Link>
+        </div>
+        <div className="text">
+            <p>2021, Ilolio Music Lab. Created by Heptanome.</p>
+            <p style={{fontSize: 10}}>Stefan Ristovski, Alexandre Bremard, Emma Neiss, Yann Dupont, Alexis Shan Yan, Iyad Tout, Aydin Akaydin</p>
         </div>
 
         </body>
