4b757358309cc9f1bbe49ddfa09d6e3850297ada
thestefanristovski <stefan.ristovski1@gmail.com>
2021-04-30 09:07:45 +0200

    working sliders in Scratch5Comp.js - get values from state

--- front/src/components/Scratch5Comp.js
+++ front/src/components/Scratch5Comp.js
@@ -19,6 +19,7 @@ import Navbar from "./navbar";
 import {toast, Toaster} from "react-hot-toast";
 import ProgressButton from "react-progress-button";
 import '../button.css'
+import Slider, {Handle, SliderTooltip} from "rc-slider";
 
 
 const styleList = [alternative, disco, electronic, hiphop, indie, jazz, rock];
@@ -32,7 +33,19 @@ export default class Scratch5Comp extends Component{
             hasResult:false,
             downloadLink: '',
             fileName: 'New Creation',
-            buttonState: ''
+            buttonState: '',
+            chords: 0,
+            style: 0,
+            melodyDrums: 0,
+            melodyPiano: 0,
+            melodyBass: 0,
+            melodyGuitar: 0,
+            melodyStrings: 0,
+            grooveDrums: 0,
+            groovePiano: 0,
+            grooveBass: 0,
+            grooveGuitar: 0,
+            grooveStrings: 0,
         }
         this.generateFile = this.generateFile.bind(this);
     }
@@ -73,6 +86,21 @@ export default class Scratch5Comp extends Component{
     }
 
     render() {
+        const handle = props => {
+            const { value, dragging, index, ...restProps } = props;
+            return (
+                <SliderTooltip
+                    prefixCls="rc-slider-tooltip"
+                    overlay={`${value}`}
+                    visible={dragging}
+                    placement="top"
+                    key={index}
+                >
+                    <Handle value={value} {...restProps} />
+                </SliderTooltip>
+            );
+        };
+
         const selectedStyle = {
             color: 'white',
             backgroundColor: 'black',
@@ -91,6 +119,61 @@ export default class Scratch5Comp extends Component{
                     <a style={selectedStyle}>5 instruments</a><Link to="/scratch1">1 instrument</Link>
                     <p><br/></p>
                     <ResultTile isLoading={this.state.isLoading} downloadLink={this.state.downloadLink} fileName={this.state.fileName} hasResult={this.state.hasResult}></ResultTile>
+                    <h5>Options</h5>
+                    <div className="maxislider">
+                        <h6>Chords</h6>
+                        <Slider min={-10} max={10} defaultValue={0} handle={handle} step={1} onChange={value => {this.setState({chords: value})}} />
+                    </div>
+                    <div className="maxislider">
+                        <h6>Style</h6>
+                        <Slider min={-10} max={10} defaultValue={0} handle={handle} step={1} onChange={value => {this.setState({style: value})}}/>
+                    </div>
+                    <h6>Melody</h6>
+                    <div className="files">
+                        <div className="minislider">
+                            <p>Drums</p>
+                            <Slider min={-10} max={10} defaultValue={0} handle={handle} step={1} onChange={value => {this.setState({melodyDrums: value})}}/>
+                        </div>
+                        <div className="minislider">
+                            <p>Piano</p>
+                            <Slider min={-10} max={10} defaultValue={0} handle={handle} step={1} onChange={value => {this.setState({melodyPiano: value})}}/>
+                        </div>
+                        <div className="minislider">
+                            <p>Guitar</p>
+                            <Slider min={-10} max={10} defaultValue={0} handle={handle} step={1} onChange={value => {this.setState({melodyGuitar: value})}}/>
+                        </div>
+                        <div className="minislider">
+                            <p>Bass</p>
+                            <Slider min={-10} max={10} defaultValue={0} handle={handle} step={1} onChange={value => {this.setState({melodyBass: value})}}/>
+                        </div>
+                        <div className="minislider">
+                            <p>Strings</p>
+                            <Slider min={-10} max={10} defaultValue={0} handle={handle} step={1} onChange={value => {this.setState({melodyStrings: value})}}/>
+                        </div>
+                    </div>
+                    <h6>Groove</h6>
+                    <div className="files">
+                        <div className="minislider">
+                            <p>Drums</p>
+                            <Slider min={-10} max={10} defaultValue={0} handle={handle} step={1} onChange={value => {this.setState({grooveDrums: value})}}/>
+                        </div>
+                        <div className="minislider">
+                            <p>Piano</p>
+                            <Slider min={-10} max={10} defaultValue={0} handle={handle} step={1} onChange={value => {this.setState({groovePiano: value})}}/>
+                        </div>
+                        <div className="minislider">
+                            <p>Guitar</p>
+                            <Slider min={-10} max={10} defaultValue={0} handle={handle} step={1} onChange={value => {this.setState({grooveGuitar: value})}}/>
+                        </div>
+                        <div className="minislider">
+                            <p>Bass</p>
+                            <Slider min={-10} max={10} defaultValue={0} handle={handle} step={1} onChange={value => {this.setState({grooveBass: value})}}/>
+                        </div>
+                        <div className="minislider">
+                            <p>Strings</p>
+                            <Slider min={-10} max={10} defaultValue={0} handle={handle} step={1} onChange={value => {this.setState({grooveStrings: value})}}/>
+                        </div>
+                    </div>
                     <p><br/></p>
                     <ProgressButton onClick={this.generateFile} state={this.state.buttonState}>
                         Generate
