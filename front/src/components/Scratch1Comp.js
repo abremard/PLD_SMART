0d7f40759b4f6fe06417c89a1bdeec2843ebd89e
thestefanristovski <stefan.ristovski1@gmail.com>
2021-05-04 10:08:52 +0200

    Scratch1Comp.js UI modifications done

--- front/src/components/Scratch1Comp.js
+++ front/src/components/Scratch1Comp.js
@@ -17,6 +17,7 @@ import rock from "../images/rock.png"
 
 //import 'react-image-picker/dist/index.css'
 import '../imagepicker.css'
+import ReactAutocomplete from "react-autocomplete";
 
 const styleList = [alternative, disco, electronic, hiphop, indie, jazz, rock];
 
@@ -34,7 +35,10 @@ export default class Scratch1Comp extends Component{
             hasResult:false,
             downloadLink: '',
             fileName: 'New Creation',
-            length: null
+            length: null,
+            songs: ['hello', 'hey', 'hello'],
+            selectedSongs: [],
+            value: '',
         }
         this.onPick = this.onPick.bind(this);
         this.generateFile = this.generateFile.bind(this);
@@ -42,8 +46,7 @@ export default class Scratch1Comp extends Component{
         this.onRemoveArtist = this.onRemoveArtist.bind(this);
         this.onSelectInstrument = this.onSelectInstrument.bind(this);
         this.onRemoveInstrument = this.onRemoveInstrument.bind(this);
-
-
+        this.handleClickChip = this.handleClickChip.bind(this);
     }
 
 
@@ -76,6 +79,15 @@ export default class Scratch1Comp extends Component{
         })
     }
 
+    handleClickChip(item) {
+        console.log(item);
+        var tempList = this.state.selectedSongs;
+        tempList = tempList.filter(x=> x!= item);
+        this.setState({
+            selectedSongs: tempList,
+        });
+    }
+
     generateFile() {
         //call code to generate file and get download link
         //wait until complete
@@ -106,6 +118,18 @@ export default class Scratch1Comp extends Component{
                     <p><br/></p>
                     <ResultTile isLoading={this.state.isLoading} downloadLink={this.state.downloadLink} fileName={this.state.fileName} hasResult={this.state.hasResult}></ResultTile>
                     <h5>Options</h5>
+                    <div className="scratch1">
+                        <h6>Choose an instrument</h6>
+                        <Multiselect
+                            options={this.state.instrumentOptions} // Options to display in the dropdown
+                            onSelect={this.onSelectInstrument} // Function will trigger on select event
+                            onRemove={this.onRemoveInstrument} // Function will trigger on remove event
+                            displayValue="name" // Property name to display in the dropdown op
+                            singleSelect
+                            id="css_custom"
+                            style={ {multiselectContainer: {width: '600px'}, searchBox:{color: 'black', border: 'solid white 2px', borderRadius:'0px'}, optionContainer: {backgroundColor: 'black', fontFamily: 'Arial', border: 'solid white 1px', borderRadius: '0px'}, chips: {backgroundColor: '#6EC3F4', fontFamily: 'Arial'}, } }
+                        />
+                    </div>
                     <h6>Choose your style</h6>
                     <ImagePicker
                         images ={styleList.map((image, i) => ({src: image, value: i}))}
@@ -122,24 +146,56 @@ export default class Scratch1Comp extends Component{
                             id="css_custom"
                             style={ {multiselectContainer: {width: '600px'}, searchBox:{color: 'black', border: 'solid white 2px', borderRadius:'0px'}, optionContainer: {backgroundColor: 'black', fontFamily: 'Arial', border: 'solid white 1px', borderRadius: '0px'}, chips: {backgroundColor: '#6EC3F4', fontFamily: 'Arial'}, } }
                         />
-                        <h6>Choose an instrument</h6>
-                        <Multiselect
-                            options={this.state.instrumentOptions} // Options to display in the dropdown
-                            onSelect={this.onSelectInstrument} // Function will trigger on select event
-                            onRemove={this.onRemoveInstrument} // Function will trigger on remove event
-                            displayValue="name" // Property name to display in the dropdown op
-                            singleSelect
-                            id="css_custom"
-                            style={ {multiselectContainer: {width: '600px'}, searchBox:{color: 'black', border: 'solid white 2px', borderRadius:'0px'}, optionContainer: {backgroundColor: 'black', fontFamily: 'Arial', border: 'solid white 1px', borderRadius: '0px'}, chips: {backgroundColor: '#6EC3F4', fontFamily: 'Arial'}, } }
-                        />
                         <h6>Maximum Length</h6>
                         <input type="number" name="name" onChange={value=>{this.setState({length: value})}} defaultValue={50} />
+                    </div>
+                    <h6>Choose songs</h6>
+                    <div className="files">
+                        <ReactAutocomplete
+                            menuStyle={{
+                                width: '400px',
+                                borderRadius: '3px',
+                                boxShadow: '0 0px 0px rgba(0, 0, 0, 0.1)',
+                                background: 'black',
+                                padding: '5px 0px',
+                                fontSize: '18px',
+                                fontFamily: 'Arial',
+                                border: '1px solid white',
+                                position: 'fixed',
+                                overflow: 'auto',
+                                maxHeight: '50%', // TODO: don't cheat, let it flow to the bottom
+                            }}
+                            items={this.state.songs}
+                            shouldItemRender={(item, value) => item.toLowerCase().indexOf(value.toLowerCase()) > -1}
+                            getItemValue={item => item}
+                            renderItem={(item, highlighted) =>
+                                <div
+                                    key={item.id}
+                                    style={{ backgroundColor: highlighted ? '#7038FF' : 'black'}}
+                                >
+                                    {item}
+                                </div>
+                            }
+                            value={this.state.value}
+                            onChange={e => this.setState({ value: e.target.value })}
+                            onSelect={e => {
+                                var listTemp = this.state.selectedSongs;
+                                if(!listTemp.includes(e)) {
+                                    listTemp.push(e);
+                                }
+                                this.setState({value: '', selectedSongs: listTemp});
+                            }}
+                        />
+                        {this.state.selectedSongs.map(item => (
+                            <div className="chip" onClick={this.handleClickChip.bind(this, item)}>
+                                <p>x {item}</p>
+                            </div>
+                        ))}
                     </div>
                     <h6><br/> </h6>
                     <ProgressButton onClick={this.generateFile} state={this.state.buttonState}>
                         Generate
                     </ProgressButton>
-
                     <h5> </h5>
                 </div>
             </>
