ddaa017731fdc543373b73b362e384399dfebe4a
aakaydin <pastirma97@gmail.com>
2021-05-05 10:27:49 +0200

    Changed Midi demo

--- front/src/pages/MidiDemo.js
+++ front/src/pages/MidiDemo.js
@@ -1,21 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import Player from '../components/Player'
 import Navbar from '../components/navbar'
 import ProgressButton from "react-progress-button";
+import { useParams } from "react-router";
+import {Link} from 'react-router-dom';
 
 
-import '@magenta/music';
-import 'focus-visible';
+const superagent = require('superagent');
+
+// jazz.mid
 
 // add navbar etc here but for the moment player overlaps everything
-export default function MidiDemo() {
+export default function MidiDemo(props) {
 
-    return(
+    let {url} = useParams();
+    url = 'https://cdn.jsdelivr.net/gh/cifkao/html-midi-player@2b12128/' + url;
+    console.log(url)
+
+    return( 
        <div>
             <Navbar/>
-            <Player url="https://cdn.jsdelivr.net/gh/cifkao/html-midi-player@2b12128/jazz.mid"/>
-            <ProgressButton >
-                        download
-            </ProgressButton>
+            <Link to="/studio">back to studio</Link>
+            <Player url={url}/>
        </div>
     )
 }
