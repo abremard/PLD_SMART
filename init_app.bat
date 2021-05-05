f4549eb153cadf7e24018d3dad8dab52685ee25a
Yumahey <yanndupont0@gmail.com>
2021-05-05 09:57:32 +0200

    scripts pour run l'appli

---
+++ init_app.bat
@@ -0,0 +1,11 @@
+pip install flask
+cd front
+npm install
+npm install --save html-midi-player
+npm install --save focus-visible
+npm install --save prop-types
+npm install --save react
+npm install --save react-dom
+npm install --save dangerously-set-html-content
+npm install --save @magenta/music
+pip install firebase_admin
